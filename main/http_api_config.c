f24a7f16f97d088fd0e569f047885e5c72672c98
Prinsx-py <skyprinszeurie@gmail.com>
2025-06-05 09:07:33 +0800

    Create http_api_config.c

---
+++ main/http_api_config.c
@@ -0,0 +1,4 @@
+esp_err_t ap_config_get_handler(httpd_req_t *req) { /* as in previous answer */ }
+esp_err_t ap_config_post_handler(httpd_req_t *req) { /* as in previous answer */ }
+esp_err_t sta_list_get_handler(httpd_req_t *req) { /* return STA list as JSON */ }
+esp_err_t sta_list_post_handler(httpd_req_t *req) { /* add/del/blacklist STA */ }
