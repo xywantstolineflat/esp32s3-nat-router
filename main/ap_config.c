d65ae8a08efe356eb033fb057bde89dee73db826
Prinsx-py <skyprinszeurie@gmail.com>
2025-06-05 09:11:33 +0800

    Update ap_config.c

--- main/ap_config.c
+++ main/ap_config.c
@@ -1,19 +1,27 @@
+#include <string.h>
 #include "nvs_flash.h"
 #include "esp_wifi.h"
-#include <string.h>
+#include "esp_log.h"
 
-#define NVS_NS_AP "ap_conf"
-void ap_config_save(const char *ssid, const char *pass) {
+#define NVS_AP_NAMESPACE "ap_config"
+
+void ap_config_save(const char *ssid, const char *password) {
     nvs_handle_t nvs;
-    nvs_open(NVS_NS_AP, NVS_READWRITE, &nvs);
-    nvs_set_str(nvs, "ap_ssid", ssid);
-    nvs_set_str(nvs, "ap_pass", pass);
-    nvs_commit(nvs); nvs_close(nvs);
+    if (nvs_open(NVS_AP_NAMESPACE, NVS_READWRITE, &nvs) == ESP_OK) {
+        nvs_set_str(nvs, "ssid", ssid);
+        nvs_set_str(nvs, "password", password);
+        nvs_commit(nvs);
+        nvs_close(nvs);
+    }
 }
-void ap_config_load(char *ssid, size_t ssid_len, char *pass, size_t pass_len) {
+
+void ap_config_load(char *ssid, size_t ssid_len, char *password, size_t pass_len) {
     nvs_handle_t nvs;
-    nvs_open(NVS_NS_AP, NVS_READONLY, &nvs);
-    nvs_get_str(nvs, "ap_ssid", ssid, &ssid_len);
-    nvs_get_str(nvs, "ap_pass", pass, &pass_len);
-    nvs_close(nvs);
+    strcpy(ssid, "ESP32-NAT");
+    strcpy(password, "esp32pass");
+    if (nvs_open(NVS_AP_NAMESPACE, NVS_READONLY, &nvs) == ESP_OK) {
+        nvs_get_str(nvs, "ssid", ssid, &ssid_len);
+        nvs_get_str(nvs, "password", password, &pass_len);
+        nvs_close(nvs);
+    }
 }
